ae3d2b15afb48facda25198d66b8faaf1ade7aed
bx22cpez <76233353+bx22cpez@users.noreply.github.com>
2022-11-13 21:29:15 -0500

    Add files via upload

---
+++ TransferData.py
@@ -0,0 +1,19 @@
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+            for root, dirs, files in os.walk(file_from):
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+
+            with open(local_path, 'rb') as f:
+                dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
